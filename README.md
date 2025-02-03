5dc3561aa7a588eb0dc1ea2ad8d52c60c1055857
Aman Kumar <amancollege04@gmail.com>
2025-02-03 11:36:28 +0530

    Setup: Project setup with tailwind css and app write

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# BlogApp
+<h1>Blog App</h1>
+<h2>Frontend: React JS & Tailwind CSS</h2>
+<h2>Backend: AppWrite</h2>
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h2 className='bg-black text-yellow-400 text-center pt-4 pb-4 font-bold'>Blog App</h2>
+    </>
+  )
+}
+
+export default App
