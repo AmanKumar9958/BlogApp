0ea34e4d9f04f4a796e5d671126a8467269890e4
Aman Kumar <amancollege04@gmail.com>
2025-03-05 13:10:14 +0530

    Add: Countdown component and remove old timer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import './timer.css'
+import CountDown from './components/CountDown'
 
 const App = () => {
   return (
     <div className='flex justify-center items-center h-screen w-full flex-col gap-4 bg-gray-800 text-white'>
       <h2 className='text-6xl font-bold'>InkFlow</h2>
-      <h3 className='text-4xl font-semibold'>Coming Soon <span className='timer'>⌛</span></h3>
+      <CountDown />
     </div>
   )
 }
---
+++ src/components/CountDown.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import './timer.css'
+
+const CountDown = () => {
+    const calculateTimeLeft = () => {
+        const targetDate = new Date("2025-03-12T00:00:00").getTime()    // Set the date we're counting down to in this format: YYYY-MM-DDTHH:MM:SS || launch date
+        const currentDate = new Date().getTime()    // Get the current date
+        const difference = targetDate - currentDate    // Calculate the difference between the target and current date
+
+        if(difference > 0){
+            return{
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),   // Calculate days left
+                hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),   // Calculate hours left
+                minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),   // Calculate minutes left
+                seconds: Math.floor((difference % (1000 * 60)) / 1000)   // Calculate seconds left
+            };
+        } else{
+            return{
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0
+            };
+        }
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());  // Set the initial time left
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+    }, []);
+
+    return (
+        <div className='flex flex-col items-center justify-center gap-4'>
+            <h3 className='text-4xl font-semibold'>Coming Soon <span className='timer'>⌛</span></h3>
+            <div className='countdown text-3xl font-semibold'>
+                {timeLeft.days}d : {timeLeft.hours}h : {timeLeft.minutes}m : {timeLeft.seconds}s
+            </div>
+        </div>
+    )
+}
+
+export default CountDown
